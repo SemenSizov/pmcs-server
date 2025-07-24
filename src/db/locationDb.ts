6d5a71ece5aa100977f6fa4881954d475d805305
Sam Syzov <sizov.semen@gmail.com>
2025-07-24 17:59:35 +0300

    refactor locatino db

--- src/db/locationDb.ts
+++ src/db/locationDb.ts
@@ -1,41 +1,31 @@
-import {pool} from './pool'
-import { Location, LocationDTO } from '../models/location'
-import { Logging } from '../utils/logger'
+import { pool } from './pool';
+import { Location, LocationDTO } from '../models/location';
+import { logger } from '../utils/logger';
 
-const logger = Logging.getInstance()
 
-export async function storeLocation(name: string){
+export const insertLocation = async (name: string) => {
     try {
-        return pool.execute('INSERT INTO location (name) VALUES (?)', [name]) 
+        return pool.execute('INSERT INTO location (name) VALUES (?)', [name]);
     } catch (e) {
-        logger.logError(`Failed to store location ${e}`)
-        throw e
+        logger.logError(`Failed to store location ${e}`);
+        throw e;
     }
-}
+};
 
-export async function eraseLocation(locationId: number) {
+export const deleteLocation = async (locationId: number) => {
     try {
-        return pool.execute('DELETE FROM location where id = ?', [locationId])
-    } catch (e){
-        logger.logError(`Failed to delete location, id=${locationId}. ${e}`)
-    }
-}
-
-export async function selectAllLocations() {
-    try {
-        const [rows, fields] = await pool.query('SELECT * FROM location')
-        return rows as Location[]
+        return pool.execute('DELETE FROM location where id = ?', [locationId]);
     } catch (e) {
-        logger.logError(`Failed to get all locations ${e}`)
-        throw e
+        logger.logError(`Failed to delete location, id=${locationId}. ${e}`);
     }
-}
+};
 
-export async function name(location: LocationDTO) {
- try {
-
- }  catch (e){
-        console.log('Failed to get locations', e)
-        throw e
- } 
-}
+export const selectAllLocations = async () => {
+    try {
+        const [rows, fields] = await pool.query('SELECT * FROM location');
+        return rows as Location[];
+    } catch (e) {
+        logger.logError(`Failed to get all locations ${e}`);
+        throw e;
+    }
+};
